da17677d5345bf11e2a2882d674019764749e15d
Ben Fortuna <bfortuna@odecee.com.au>
2022-03-31 19:54:20 +1100

    Build configuration refactor

---
+++ diagram.py
@@ -0,0 +1,24 @@
+from diagrams import Diagram, Cluster, Edge
+from diagrams.aws.compute import ECS
+from diagrams.aws.network import APIGateway, CloudMap, ELB
+from diagrams.generic.blank import Blank
+
+with Diagram("AWS CodeDeploy App", show=False, direction="TB"):
+
+    api_gw = APIGateway("api gateway")
+
+    cloud_map = CloudMap("cloud map")
+    api_gw << cloud_map
+
+    with Cluster("vpc"):
+
+        target_group = ELB("target_group (+)")
+
+    with Cluster("ecs"):
+
+     service = ECS("ecs service")
+     api_gw >> service >> cloud_map
+
+     service >> Edge(label="container:port (+)") >> target_group
+
+    Blank("+ 1..n")
